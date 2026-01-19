762e5b213a24036c9d41513322afe73c418ec0cf
yomikui <meow09217@gmail.com>
2026-01-19 01:27:08 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 --// CONFIG
 local SCRIPT_NAME = "PaidGrade"
-local KEY_URL = "https://paidgrade-api.vercel.app/validate"
+local KEY_URL = "https://paidgrade-api.vercel.app/api/validate"
 local SCRIPT_RAW_URL = "https://raw.githubusercontent.com/yomikui/paidgrade-script/main/script.lua" -- main script
 
 local HttpService = game:GetService("HttpService")
@@ -68,3 +68,4 @@ end
 pcall(function()
     loadstring(game:HttpGet(https://raw.githubusercontent.com/yomikui/paidgrade-script/main/script.lua))()
 end)
+
